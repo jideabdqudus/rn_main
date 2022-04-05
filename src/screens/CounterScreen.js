d14257181cf700bf6fcd71b61225a8194cb994e6
jideabdqudus <jideabdqudus@gmail.com>
2022-04-05 22:04:08 +0100

    Counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,23 +1,38 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import React, { useState } from "react";
 
 const CounterScreen = () => {
   const [counter, setCounter] = useState(0);
+  const [text, setText] = useState("");
   const onHandleIncrease = () => {
     setCounter(counter + 1);
   };
   const onHandleDecrease = () => {
-    setCounter(counter--);
+    setCounter(counter - 1);
   };
   return (
     <View>
       <Button title="Increase" onPress={onHandleIncrease} />
       <Button title="Decrease" onPress={onHandleDecrease} />
       <Text>Current Count: {counter}</Text>
+      <TextInput
+        style={styles.textInput}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={text}
+        onChangeText={(value) => setText(value)}
+      />
+      <Text>My Input: {text}</Text>
     </View>
   );
 };
 
 export default CounterScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textInput: {
+    margin: 15,
+    borderWidth: 2,
+    borderColor: "black",
+  },
+});
